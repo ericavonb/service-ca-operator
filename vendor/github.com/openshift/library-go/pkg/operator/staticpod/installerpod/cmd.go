862979bd12fe868ded3910f88cfdf08b03870e74
EvB <evb@redhat.com>
2019-02-04 12:06:10 -0500

    chore(glide/vendor): bump deps

--- vendor/github.com/openshift/library-go/pkg/operator/staticpod/installerpod/cmd.go
+++ vendor/github.com/openshift/library-go/pkg/operator/staticpod/installerpod/cmd.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path"
 	"strings"
+	"time"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/golang/glog"
@@ -13,10 +14,13 @@ import (
 	"github.com/spf13/pflag"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	utilwait "k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 
 	"github.com/openshift/library-go/pkg/config/client"
+	"github.com/openshift/library-go/pkg/operator/resource/resourceread"
 )
 
 type InstallOptions struct {
@@ -24,19 +28,31 @@ type InstallOptions struct {
 	KubeConfig string
 	KubeClient kubernetes.Interface
 
-	DeploymentID string
-	Namespace    string
+	Revision  string
+	Namespace string
 
-	PodConfigMapNamePrefix string
-	SecretNamePrefixes     []string
-	ConfigMapNamePrefixes  []string
+	PodConfigMapNamePrefix        string
+	SecretNamePrefixes            []string
+	OptionalSecretNamePrefixes    []string
+	ConfigMapNamePrefixes         []string
+	OptionalConfigMapNamePrefixes []string
 
 	ResourceDir    string
 	PodManifestDir string
+
+	PodMutationFns []PodMutationFunc
 }
+
+// PodMutationFunc is a function that has a chance at changing the pod before it is created
+type PodMutationFunc func(pod *corev1.Pod) error
 
 func NewInstallOptions() *InstallOptions {
 	return &InstallOptions{}
+}
+
+func (o *InstallOptions) WithPodMutationFn(podMutationFn PodMutationFunc) *InstallOptions {
+	o.PodMutationFns = append(o.PodMutationFns, podMutationFn)
+	return o
 }
 
 func NewInstaller() *cobra.Command {
@@ -68,11 +84,13 @@ func NewInstaller() *cobra.Command {
 
 func (o *InstallOptions) AddFlags(fs *pflag.FlagSet) {
 	fs.StringVar(&o.KubeConfig, "kubeconfig", o.KubeConfig, "kubeconfig file or empty")
-	fs.StringVar(&o.DeploymentID, "deployment-id", o.DeploymentID, "identifier for this particular installation instance.  For example, a counter or a hash")
+	fs.StringVar(&o.Revision, "revision", o.Revision, "identifier for this particular installation instance.  For example, a counter or a hash")
 	fs.StringVar(&o.Namespace, "namespace", o.Namespace, "namespace to retrieve all resources from and create the static pod in")
 	fs.StringVar(&o.PodConfigMapNamePrefix, "pod", o.PodConfigMapNamePrefix, "name of configmap that contains the pod to be created")
 	fs.StringSliceVar(&o.SecretNamePrefixes, "secrets", o.SecretNamePrefixes, "list of secret names to be included")
 	fs.StringSliceVar(&o.ConfigMapNamePrefixes, "configmaps", o.ConfigMapNamePrefixes, "list of configmaps to be included")
+	fs.StringSliceVar(&o.OptionalSecretNamePrefixes, "optional-secrets", o.OptionalSecretNamePrefixes, "list of optional secret names to be included")
+	fs.StringSliceVar(&o.OptionalConfigMapNamePrefixes, "optional-configmaps", o.OptionalConfigMapNamePrefixes, "list of optional configmaps to be included")
 	fs.StringVar(&o.ResourceDir, "resource-dir", o.ResourceDir, "directory for all files supporting the static pod manifest")
 	fs.StringVar(&o.PodManifestDir, "pod-manifest-dir", o.PodManifestDir, "directory for the static pod manifest")
 }
@@ -90,17 +108,14 @@ func (o *InstallOptions) Complete() error {
 }
 
 func (o *InstallOptions) Validate() error {
-	if len(o.DeploymentID) == 0 {
-		return fmt.Errorf("--deployment-id is required")
+	if len(o.Revision) == 0 {
+		return fmt.Errorf("--revision is required")
 	}
 	if len(o.Namespace) == 0 {
 		return fmt.Errorf("--namespace is required")
 	}
 	if len(o.PodConfigMapNamePrefix) == 0 {
 		return fmt.Errorf("--pod is required")
-	}
-	if len(o.SecretNamePrefixes) == 0 {
-		return fmt.Errorf("--secrets is required")
 	}
 	if len(o.ConfigMapNamePrefixes) == 0 {
 		return fmt.Errorf("--configmaps is required")
@@ -114,18 +129,31 @@ func (o *InstallOptions) Validate() error {
 }
 
 func (o *InstallOptions) nameFor(prefix string) string {
-	return fmt.Sprintf("%s-%s", prefix, o.DeploymentID)
+	return fmt.Sprintf("%s-%s", prefix, o.Revision)
 }
 
 func (o *InstallOptions) prefixFor(name string) string {
-	return name[0 : len(name)-len(fmt.Sprintf("-%s", o.DeploymentID))]
+	return name[0 : len(name)-len(fmt.Sprintf("-%s", o.Revision))]
 }
 
 func (o *InstallOptions) copyContent() error {
 	// gather all secrets
 	secrets := []*corev1.Secret{}
 	for _, currPrefix := range o.SecretNamePrefixes {
+		glog.Infof("getting secrets/%s -n %s", o.nameFor(currPrefix), o.Namespace)
 		val, err := o.KubeClient.CoreV1().Secrets(o.Namespace).Get(o.nameFor(currPrefix), metav1.GetOptions{})
+		if err != nil {
+			return err
+		}
+		secrets = append(secrets, val)
+	}
+	for _, currPrefix := range o.OptionalSecretNamePrefixes {
+		glog.Infof("getting optional secrets/%s -n %s", o.nameFor(currPrefix), o.Namespace)
+		val, err := o.KubeClient.CoreV1().Secrets(o.Namespace).Get(o.nameFor(currPrefix), metav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			glog.Infof("missing optional secrets/%s -n %s", o.nameFor(currPrefix), o.Namespace)
+			continue
+		}
 		if err != nil {
 			return err
 		}
@@ -135,7 +163,20 @@ func (o *InstallOptions) copyContent() error {
 	// gather all configmaps
 	configmaps := []*corev1.ConfigMap{}
 	for _, currPrefix := range o.ConfigMapNamePrefixes {
+		glog.Infof("getting configmaps/%s -n %s", o.nameFor(currPrefix), o.Namespace)
 		val, err := o.KubeClient.CoreV1().ConfigMaps(o.Namespace).Get(o.nameFor(currPrefix), metav1.GetOptions{})
+		if err != nil {
+			return err
+		}
+		configmaps = append(configmaps, val)
+	}
+	for _, currPrefix := range o.OptionalConfigMapNamePrefixes {
+		glog.Infof("getting optional configmaps/%s -n %s", o.nameFor(currPrefix), o.Namespace)
+		val, err := o.KubeClient.CoreV1().ConfigMaps(o.Namespace).Get(o.nameFor(currPrefix), metav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			glog.Infof("missing optional configmaps/%s -n %s", o.nameFor(currPrefix), o.Namespace)
+			continue
+		}
 		if err != nil {
 			return err
 		}
@@ -143,25 +184,29 @@ func (o *InstallOptions) copyContent() error {
 	}
 
 	// gather pod
+	glog.Infof("getting pod configmaps/%s -n %s", o.nameFor(o.PodConfigMapNamePrefix), o.Namespace)
 	podConfigMap, err := o.KubeClient.CoreV1().ConfigMaps(o.Namespace).Get(o.nameFor(o.PodConfigMapNamePrefix), metav1.GetOptions{})
 	if err != nil {
 		return err
 	}
 	podContent := podConfigMap.Data["pod.yaml"]
-	podContent = strings.Replace(podContent, "DEPLOYMENT_ID", o.DeploymentID, -1)
+	podContent = strings.Replace(podContent, "REVISION", o.Revision, -1)
 
 	// write secrets, configmaps, static pods
 	resourceDir := path.Join(o.ResourceDir, o.nameFor(o.PodConfigMapNamePrefix))
+	glog.Infof("creating dir %q", resourceDir)
 	if err := os.MkdirAll(resourceDir, 0755); err != nil {
 		return err
 	}
 	for _, secret := range secrets {
 		contentDir := path.Join(resourceDir, "secrets", o.prefixFor(secret.Name))
+		glog.Infof("creating dir %q", contentDir)
 		if err := os.MkdirAll(contentDir, 0755); err != nil {
 			return err
 		}
 		for filename, content := range secret.Data {
 			// TODO fix permissions
+			glog.Infof("writing secret file %q", path.Join(contentDir, filename))
 			if err := ioutil.WriteFile(path.Join(contentDir, filename), content, 0644); err != nil {
 				return err
 			}
@@ -169,10 +214,12 @@ func (o *InstallOptions) copyContent() error {
 	}
 	for _, configmap := range configmaps {
 		contentDir := path.Join(resourceDir, "configmaps", o.prefixFor(configmap.Name))
+		glog.Infof("creating dir %q", contentDir)
 		if err := os.MkdirAll(contentDir, 0755); err != nil {
 			return err
 		}
 		for filename, content := range configmap.Data {
+			glog.Infof("writing configmap file %q", path.Join(contentDir, filename))
 			if err := ioutil.WriteFile(path.Join(contentDir, filename), []byte(content), 0644); err != nil {
 				return err
 			}
@@ -184,9 +231,21 @@ func (o *InstallOptions) copyContent() error {
 	}
 
 	// copy static pod
+	glog.Infof("creating dir %q", o.PodManifestDir)
 	if err := os.MkdirAll(o.PodManifestDir, 0755); err != nil {
 		return err
 	}
+
+	for _, fn := range o.PodMutationFns {
+		glog.V(2).Infof("customizing static pod")
+		pod := resourceread.ReadPodV1OrDie([]byte(podContent))
+		if err := fn(pod); err != nil {
+			return err
+		}
+		podContent = resourceread.WritePodV1OrDie(pod)
+	}
+	glog.Infof("writing static pod %q", path.Join(o.PodManifestDir, podFileName))
+	glog.Infof("writing static pod\n%s", podContent)
 	if err := ioutil.WriteFile(path.Join(o.PodManifestDir, podFileName), []byte(podContent), 0644); err != nil {
 		return err
 	}
@@ -195,8 +254,23 @@ func (o *InstallOptions) copyContent() error {
 }
 
 func (o *InstallOptions) Run() error {
-	if err := o.copyContent(); err != nil {
-		return err
+	// ~2 min total waiting
+	backoff := utilwait.Backoff{
+		Duration: time.Second,
+		Factor:   1.5,
+		Steps:    11,
+	}
+	attempts := 0
+	err := utilwait.ExponentialBackoff(backoff, func() (bool, error) {
+		attempts += 1
+		if copyErr := o.copyContent(); copyErr != nil {
+			fmt.Fprintf(os.Stderr, "#%d: failed to copy content: %v", attempts, copyErr)
+			return false, nil
+		}
+		return true, nil
+	})
+	if err != nil {
+		return fmt.Errorf("error: %v", err)
 	}
 
 	// TODO wait for healthy pod status
